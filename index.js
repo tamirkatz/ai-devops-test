2cfba515d2f26d4329ae42e0f2be906df4aad3b5
tamirkatz <tamirkatz@hotmail.com>
2025-04-30 18:27:05 +0300

    feat(SCRUM-22): add /Tamir route to index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+- Content:
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Test message");
+});
+
+app.get("/api", (req, res) => {
+  res.send("ofri");
+});
+
+app.post("/Tamir", (req, res) => {
+  if (req.body["Tamir is the king"] === true) {
+    res.send("you are awesome");
+  } else {
+    res.send("you are lame !");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
