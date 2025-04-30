4c804f31bee2133b58bcfbdc18abc8d5ceeb848f
tamirkatz <tamirkatz@hotmail.com>
2025-04-30 16:24:55 +0300

    feat(AI-123): Setup basic Express server

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+- Content:
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Test message");
-});
-
-app.get("/api", (req, res) => {
-  res.send("ofri");
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+-
